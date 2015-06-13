0987ada61107d81b65c5856510f6ccd950cf1716
iamunknown2 <iamunknown2@users.noreply.github.com>
2015-06-13 14:32:20 +0800

    Updated with Git_Update.py

--- programs/countdown.js
+++ programs/countdown.js
@@ -2,11 +2,10 @@
 var oneDay = 24*60*60*1000; // hrs * mins * secs * ms
 var firstDate = new Date();
 var secondDate = new Date(2015, 5, 27); // Countdown date changed
-var days_left = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
+var days_left = Math.ceil(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay))); // Changed Math.round to Math.ceil here
 // End of code from http://stackoverflow.com/questions/16484884/how-do-i-get-the-how-many-days-until-my-next-birthday
-
 if (days_left == 0 || days_left == -1) {
-	var html = "<h2 style=\"display:inline;\">IT'S FINALLY BEGUN!</h2>\n<br>\n<iframe width=\"0\" height=\"0\" src=\"https://www.youtube.com/embed/DcwRwYcDsk4?autoplay=1&loop=1\" frameborder=\"0\"></iframe><span>(P.S song is 305 by ApproachingNirvana</span>"; // YouTube embed hack (thought of this myself). Credit to multiple sources (e.g w3schools, stackoverflow and Google Developers References) for other things, e.g autoplay.
+	var html = "<h2 style=\"display:inline;\">IT'S FINALLY BEGUN!</h2>\n<br>\n<iframe width=\"0\" height=\"0\" src=\"https://www.youtube.com/embed/DcwRwYcDsk4?autoplay=1&loop=1&playlist=DcwRwYcDsk4\" frameborder=\"0\"></iframe><span>(P.S song is 305 by ApproachingNirvana</span>"; // YouTube embed hack (thought of this myself). Credit to multiple sources (e.g w3schools, stackoverflow and Google Developers References) for other things, e.g autoplay.
 } else if (days_left > 0) {
 	var html = "<h3>" + days_left.toString() + " days left until summer holiday begins..." + "</h3>";
 } else {

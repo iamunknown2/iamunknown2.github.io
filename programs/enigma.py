13e838cb4f5253d0ef254f3b0ab942e48a81eed3
iamunknown2 <iamunknown2@users.noreply.github.com>
2016-01-01 11:00:06 +0800

    Enigma Update

--- programs/enigma.py
+++ programs/enigma.py
@@ -2,24 +2,24 @@ import string
 try: input = raw_input
 except NameError: pass
 
-alphabet = string.ascii_lowercase + " " 
+alphabet = string.ascii_lowercase + ",.!?" + " " 
 rotor_count = 15
 
-rotor1 = ["t", "z", "l", "d", "u", " ", "y", "i", "m", "c", "f", "o", "b", "q", "r", "s", "h", "k", "e", "j", "v", "g", "a", "p", "w", "x", "n"]
-rotor2 = ["p", "e", "t", "s", "m", "n", "z", "k", "d", "o", "r", "l", "x", "v", "i", "f", " ", "j", "g", "b", "c", "y", "a", "q", "h", "u", "w"]
-rotor3 = ["x", "i", "g", "e", " ", "u", "s", "r", "a", "p", "d", "v", "f", "n", "h", "j", "t", "m", "l", "z", "o", "y", "w", "c", "k", "b", "q"]
-rotor4 = ["d", "l", "z", "x", "c", "q", "s", "t", "e", " ", "n", "o", "a", "h", "g", "k", "f", "v", "b", "y", "p", "u", "m", "j", "i", "r", "w"]
-rotor5 = ["x", "h", "s", "g", "u", "l", "w", "o", "c", "b", "t", "q", "a", "m", "k", " ", "r", "v", "f", "n", "i", "j", "y", "z", "e", "d", "p"]
-rotor6 = ["g", "a", "b", "i", "o", "m", "l", "x", "y", "r", "u", "v", "q", "f", "w", "j", "s", "c", "z", "h", "t", "e", "p", "n", " ", "d", "k"]
-rotor7 = ["s", " ", "h", "c", "m", "w", "q", "f", "n", "u", "l", "t", "b", "v", "g", "j", "k", "x", "o", "a", "e", "z", "p", "d", "r", "y", "i"]
-rotor8 = ["f", "b", "s", "c", "i", "j", "m", "e", "g", "l", "q", "n", "a", " ", "t", "u", "o", "h", "d", "p", "w", "k", "v", "r", "z", "x", "y"]
-rotor9 = ["a", "h", "j", "o", "f", "d", "r", "g", "z", "k", "n", "t", " ", "w", "v", "c", "x", "b", "s", "l", "i", "m", "p", "u", "y", "q", "e"]
-rotor10 = ["t", "g", "j", "i", "l", "q", "m", "s", "p", "v", "x", "d", "a", "c", "y", "h", "o", "r", "e", "u", " ", "z", "k", "n", "w", "b", "f"]
-rotor11 = ["s", "x", "n", "q", "v", "l", " ", "o", "y", "p", "z", "w", "f", "m", "r", "u", "t", "g", "c", "j", "d", "i", "k", "a", "e", "h", "b"]
-rotor12 = ["j", "y", "m", "o", "f", "g", "s", "e", "k", " ", "n", "i", "w", "v", "x", "c", "h", "p", "t", "l", "u", "a", "r", "z", "b", "d", "q"]
-rotor13 = ["z", "l", "g", "x", "e", "j", "s", "a", "v", "i", "c", "t", "f", "w", "b", "q", "o", "u", "r", " ", "h", "d", "k", "p", "n", "m", "y"]
-rotor14 = ["o", "v", "n", "g", "m", "d", "u", "r", "f", "e", "i", "l", "x", "y", "k", "s", "a", " ", "q", "b", "t", "c", "p", "j", "h", "w", "z"]
-rotor15 = ["a", "v", "n", "g", "k", "l", "q", "j", "p", "y", "m", "i", "t", "x", "w", "r", "f", "s", " ", "c", "h", "o", "z", "u", "d", "b", "e"]
+rotor1 = ["d", "g", "!", "c", "s", "x", "l", "t", "k", "u", "n", "i", "b", "q", "y", "p", "j", "e", "z", ".", "f", " ", "w", "r", "h", "m", "v", ",", "o", "a", "?"]
+rotor2 = ["l", "v", "i", "d", "u", "w", " ", "o", "e", "r", "x", "j", "n", "g", "s", "c", "h", ".", ",", "m", "?", "p", "q", "!", "b", "k", "f", "a", "z", "y", "t"]
+rotor3 = ["w", "i", "v", "c", "j", "l", "f", "a", "o", "k", "h", "p", "n", "m", ",", "u", "r", "x", "z", "d", "s", "e", "y", " ", "b", "t", "g", "q", "!", ".", "?"]
+rotor4 = ["v", "q", "j", "d", "k", "o", "h", "t", "l", " ", "r", "p", "s", "u", "y", ",", "?", "i", "x", ".", "w", "b", "e", "n", "g", "!", "m", "a", "z", "c", "f"]
+rotor5 = ["d", "v", "c", "q", ",", "!", "j", "p", "x", ".", "?", " ", "b", "h", "k", "u", "z", "e", "n", "r", "y", "w", "l", "s", "o", "t", "a", "f", "g", "i", "m"]
+rotor6 = ["l", "r", "!", "j", "d", "x", "h", "p", "a", "u", "s", "m", ".", "w", "o", "k", "v", "c", "f", "t", "?", "y", "n", "e", " ", "g", "q", "z", ",", "i", "b"]
+rotor7 = [".", "h", "p", "y", "k", "g", "d", "c", "t", "x", "r", "e", "q", "a", ",", " ", "u", "l", "!", "o", "b", "i", "j", "f", "n", "m", "v", "w", "z", "?", "s"]
+rotor8 = ["i", "z", "m", "y", "p", "l", "w", "f", "a", "k", "q", " ", "v", "s", "e", "x", ",", "t", "!", "g", "r", "o", "j", "c", "?", ".", "h", "u", "n", "d", "b"]
+rotor9 = ["r", "o", "f", ",", "m", "x", "v", "t", "l", "w", "y", "e", "j", "i", "a", "k", "u", "b", "h", "p", "q", "n", "?", " ", "c", "!", "g", "d", "z", "s", "."]
+rotor10 = ["x", "w", "t", "!", "?", "d", "o", "c", ".", "h", "s", "e", "l", ",", "i", "k", "b", "m", "q", "u", "y", "n", "g", " ", "v", "j", "r", "a", "z", "p", "f"]
+rotor11 = ["y", "t", "a", "m", "z", "d", "o", "!", "x", "e", "q", " ", "s", "l", "u", "i", "c", "r", "?", ".", "k", "w", "g", "b", ",", "n", "p", "j", "f", "v", "h"]
+rotor12 = ["u", "z", "b", "o", "v", "h", "k", "m", "?", "c", ".", "w", "t", "q", ",", "f", "j", " ", "!", "n", "i", "r", "p", "d", "l", "g", "x", "e", "s", "y", "a"]
+rotor13 = ["z", "d", "j", "s", "w", "c", "m", "p", ",", "i", "t", "y", "q", "k", "v", "u", "r", "x", "h", "o", ".", "a", "g", "b", "f", "n", "?", "!", "l", "e", " "]
+rotor14 = ["x", "t", "b", "u", "f", "?", "k", "z", "v", "h", "y", "o", "n", "!", "c", "q", "r", ".", "w", "s", "e", " ", "m", "j", "d", "g", "a", "i", ",", "p", "l"]
+rotor15 = ["e", "c", "r", "o", "p", "b", "i", "x", "m", "j", "?", "f", "q", "g", "s", "t", " ", "h", "l", ".", "!", "w", "u", "z", "k", "n", "y", "v", "a", ",", "d"]
 
 def shuffle(lst):
 	last = len(lst)
@@ -84,7 +84,7 @@ def decrypt(codetext, position, order):
 action = input("(e) Encrypt or (d) decrypt?\n")
 if (action.lower() == "e"):
 	plaintext = input("Enter plaintext to encrypt\n")
-	position = input("Enter position (any combination of 15 alphabet letters or spaces)\n")
+	position = input("Enter position (any combination of 15 alphabet letters or spaces/periods/commas/exclamation marks/question marks)\n")
 	order = input("Enter order as list (1 2 ... all the way to 15, but scrambled up)\n").split()
 	print(encrypt(plaintext, position, order))
 if (action.lower() == "d"):

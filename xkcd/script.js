84f05abb55ea895bed32fd44ac92b8f332226cc6
iamunknown2 <iamunknown2@users.noreply.github.com>
2016-01-08 10:50:03 +0800

    Update

--- xkcd/script.js
+++
@@ -1,8 +0,0 @@
-function getImageUrl(number)
-{
-	var stringInt = number.toString();
-	var url = "https://xkcd.com/" + stringInt + "/" + "info.0.json"
-	var object = JSON.parse($.get({url: url, success: function(data){return data;}}));
-	return object;
-}
-console.log(getImageUrl(1338));

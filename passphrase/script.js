9717579157034a7b219bfb59f944933bb4210e4e
iamunknown2 <iamunknown2@users.noreply.github.com>
2016-01-23 14:44:51 +0800

    Passphrase

--- passphrase/script.js
+++ passphrase/script.js
@@ -1,31 +1,28 @@
 function generatePassPhrase()
 {
 	var commonList = [];
-	function resultToList(result)
-	{
-	}
 	function ajaxFunction(callback)
 	{
 		$.ajax
 		(
 			{
 				url: "./2048.txt",
-				async: false,
-				success: function(result)
+				"async": false,
+				"success": function(result)
 				{
 					commonList = result.split("\n");
+					var word1_item = Math.floor(Math.random() * commonList.length);
+					var word2_item = Math.floor(Math.random() * commonList.length);
+					var word3_item = Math.floor(Math.random() * commonList.length);
+					var word4_item = Math.floor(Math.random() * commonList.length);
+					var word1 = commonList[word1_item];
+					var word2 = commonList[word2_item];
+					var word3 = commonList[word3_item];
+					var word4 = commonList[word4_item];
+					document.getElementById("passphrase-output").innerHTML = word1 + word2 + word3 + word4;
 				}
 			}
 		);
 	}
 	console.log(commonList);
-	var word1_item = Math.floor(Math.random() * commonList.length);
-	var word2_item = Math.floor(Math.random() * commonList.length);
-	var word3_item = Math.floor(Math.random() * commonList.length);
-	var word4_item = Math.floor(Math.random() * commonList.length);
-	var word1 = commonList[word1_item];
-	var word2 = commonList[word2_item];
-	var word3 = commonList[word3_item];
-	var word4 = commonList[word4_item];
-	document.getElementById("passphrase-output").innerHTML = word1 + word2 + word3 + word4;
 }

8d62618d3f56d5f6326d103b02ea5bbed1a48122
iamunknown2 <iamunknown2@users.noreply.github.com>
2016-01-23 13:00:28 +0800

    Passphrase

---
+++ passphrase/script.js
@@ -0,0 +1,23 @@
+function generatePassPhrase()
+{
+	var commonList = [];
+	$.ajax
+	(
+		{
+			url: "./2048.txt",
+			success: function(result)
+			{
+				commonList = result.split("\n");
+			}
+		}
+	);
+	var word1_item = Math.floor(Math.random() * commonList.length);
+	var word2_item = Math.floor(Math.random() * commonList.length);
+	var word3_item = Math.floor(Math.random() * commonList.length);
+	var word4_item = Math.floor(Math.random() * commonList.length);
+	var word1 = commonList[word1_item];
+	var word2 = commonList[word2_item];
+	var word3 = commonList[word3_item];
+	var word4 = commonList[word4_item];
+	document.getElementById("passphrase-output").innerHTML = word1 + word2 + word3 + word4;
+}
